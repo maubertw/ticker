7bfff48536946512825790ead5d7b3d1c073cca2
Mary Warrick <mewarrick@gmail.com>
2018-12-11 21:10:34 -0600

    optimized subscripttion functions

--- src/book.js
+++ src/book.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Row from './row'
 import Mid from './mid'
-import {open, wssIntake, wssTick } from './subscribe'
+import {open, wssIntake, wssTick, unsubscribe, date, handleWSSFeed } from './subscribe'
 
 
 
@@ -14,63 +14,33 @@ class Book extends Component {
         mid: 0,
         lastMid: 80,
         total: 0,
-        units: [],
-        isSet: false
+        units: {},
+        isSet: false,
+        sortedFeed: [],
+        totalOfPrices: 0
       }
 
   }
 
 
   componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.setState({
-        date: JSON.stringify(Date(Date.now()))
-      }),
-      1000
-    )
+    this.intervalID = date(this)
     this.socket = new WebSocket('wss://ws-feed.pro.coinbase.com');
     open(this.socket)
-    let self = this
-    this.socket.addEventListener('message', function(event){
-      event = JSON.parse(event.data)
-      if(event.type == "snapshot" && !self.state.isSet){
-        const {splitIndex, newState, totalShares, newMidPrice} = wssIntake(event.bids.slice(0, 20))
-        self.setState({
-          splitIndex,
-          mid: newMidPrice,
-          total: totalShares,
-          units: newState,
-          isSet: true
-        })
-      }else if(event.type == "l2update" && event.changes[0][0] == "buy"){
-        //console.log(event)
-        const {splitIndex, newState, totalShares, newMidPrice} = wssTick(event.changes[0], self.state.units, self.state.splitIndex, self.state.lastMid)
-        self.setState({
-          splitIndex,
-          mid: newMidPrice,
-          total: totalShares,
-          units: newState
-        })
-       }
-    })
+    handleWSSFeed(this)
   }
-
-  // newState = [...state.slice(0, index), ...newBid, ...state.slice(index+1)]
-  //newState = [...state.slice(0, index+1), ...newBid, state.slice(index+1)]
 
 
   componentWillUnmount() {
     clearInterval(this.intervalID)
+    this.socket.send(JSON.stringify(unsubscribe))
     this.socket.close()
   }
 
 
   render (){
 
-    const { mid, lastMid, date, units } = this.state
-    const entries = Object.keys(units).sort((a, b) => {
-      return b - a
-    })
+    const { mid, lastMid, date, units, sortedFeed } = this.state
     const change = lastMid > 0 ? (((mid-lastMid)/lastMid)*100).toFixed(2) : ''
     return(
       <div className='book'>
@@ -81,18 +51,18 @@ class Book extends Component {
             <tbody>
               <span>
                  {
-                  entries.length > 0 && entries.slice(0, this.state.splitIndex).map(entry => {
-                      return <Row bid={[entry, units[entry]]} percent={(+units[entry]/this.state.total)*10} />
+                  sortedFeed.length > 0 && sortedFeed.slice(0, this.state.splitIndex).map(entry => {
+                      return <Row bid={[entry, units[entry]]} />
                   })
                 }
               </span>
               {
-                entries.length > 0 && <Mid mid={mid} isUp={mid > lastMid} change={change} />
+                sortedFeed.length > 0 && <Mid mid={mid} isUp={mid > lastMid} change={change} />
               }
               <span>
               {
-                 entries.length > 0 &&  entries.slice(this.state.splitIndex).map(entry => {
-                  return <Row bid={[entry, units[entry]]} percent={(+units[entry]/this.state.total)*10} />
+                 sortedFeed.length > 0 &&  sortedFeed.slice(this.state.splitIndex).map(entry => {
+                  return <Row bid={[entry, units[entry]]}  />
                 })
                 }
               </span>
@@ -105,5 +75,5 @@ class Book extends Component {
 
 export default Book
 
-
-
+// percent={(+units[entry]/this.state.total)*10
+//   percent={(+units[entry]/this.state.total)*10}
--- src/subscribe.js
+++ src/subscribe.js
@@ -9,25 +9,116 @@ const subscriptionMessage = {
     ]
   }
 
+  export const unsubscribe = {
+    "type": "unsubscribe",
+    "product_ids": [
+        "ETH-USD",
+    ],
+    "channels": [
+        "level2",
+    ]
+}
 
+export const date = (self) => {
+    setInterval(
+        () => self.setState({
+          date: JSON.stringify(Date(Date.now()))
+        }),
+        1000
+      )
+}
 
 export const open = (socket) => socket.addEventListener('open', function (event) {
       socket.send(JSON.stringify(subscriptionMessage));
 
   });
 
-//need to make sure the type is correct in the search bids function while continuing to work on the tick logic.  zeros are taken care of, now we have duplicates
+const insertBid = (feed, bid) => {
+    let closestIndex = Math.floor(feed.length/2)
+    let low = 0
+    let high = feed.length-1
+    let mid = Math.floor(feed.length/2)
+    while(low < high){
+
+      if( Math.abs(+feed[mid] - (+bid)) < Math.abs(+feed[closestIndex] - (+bid)) ){
+          closestIndex = mid
+      }
+      if(+feed[mid] > +bid){
+          high = mid - 1
+          mid = Math.floor(high-mid/2)
+      }else{
+          low = mid + 1
+          mid = Math.floor(high+low/ 2)
+      }
+    }
+    let sortedFeed = [...feed.slice(0, closestIndex, bid, ...feed.slice(closestIndex))]
+    return sortedFeed
+}
+
+
+export const handleWSSFeed = (self) => {
+  self.socket.addEventListener('message', function(event){
+        event = JSON.parse(event.data)
+        if(event.type == "snapshot" && !self.state.isSet){
+          const {splitIndex, newState, totalShares, newMidPrice, sortedFeed, totalOfPrices } = wssIntake(event.bids.slice(0, 200))
+          self.setState({
+            splitIndex,
+            mid: newMidPrice,
+            total: totalShares,
+            units: newState,
+            isSet: true,
+            sortedFeed,
+            totalOfPrices
+          })
+        }else if(event.type == "l2update" && event.changes[0][0] == "buy"){
+
+          const {splitIndex, newState, newMidPrice, sortedFeed, totalOfPrices, totalShares} = wssTick
+          (event.changes[0], self.state.units, self.state.splitIndex, self.state.lastMid, self.state.sortedFeed, self.state.totalOfPrices, self.state.total)
+          self.setState({
+            splitIndex,
+            mid: newMidPrice,
+            total: totalShares,
+            units: newState,
+            sortedFeed,
+            totalOfPrices
+          })
+         }
+      })
+}
+
+
+function findMedian(allPrice, totalSize, bid, change, length, midPrice, type){
+    let newTotalShares
+    let newTotalPrice
+    let newMid
+    if(type === 'delete'){
+        newTotalPrice = allPrice - (+bid[0])
+        newTotalShares = totalSize - (+bid[1])
+        newMid = newTotalPrice/length
+    }else if(type === 'update'){
+      newTotalShares = +bid[1] > +change[1]
+        ? totalSize - (+bid[1] - +change[1])
+        : totalSize + (+change[1] - +bid[1])
+      newMid = midPrice
+      newTotalPrice = allPrice
+    }else if(type === 'add'){
+        newTotalShares = totalSize + +change[1]
+        newTotalPrice = allPrice + +change[0]
+        newMid = newTotalPrice/(length+1)
+    }
+    return {newTotalPrice, newTotalShares, newMid}
+}
 
 
 const findTotalSharesAndMedianPrice = bidList => {
     let totalShares = 0
-    let midPrice = 0
+    let totalOfPrices = 0
     bidList.forEach(bid => {
         totalShares += (+bid[1])
-        midPrice += (+bid[0])
+        totalOfPrices += (+bid[0])
     })
-    midPrice = midPrice/bidList.length
-    return { midPrice, totalShares }
+    let midPrice = totalOfPrices/bidList.length
+    return { midPrice, totalShares, totalOfPrices}
 }
 
 
@@ -42,39 +133,60 @@ export function wssIntake (feed){
         newState[key] = value
     })
     let newSharesAndMedian = findTotalSharesAndMedianPrice(feed)
+    let totalOfPrices = newSharesAndMedian.totalOfPrices
     midPrice = newSharesAndMedian.midPrice
     totalShares = newSharesAndMedian.totalShares
     splitIndex = feed.length/2
-    return {splitIndex, newState, totalShares, newMidPrice: midPrice}
+    let sortedFeed = Object.keys(newState).sort((a, b) => {
+        return b - a
+      })
+    return {splitIndex, newState, newMidPrice: midPrice, sortedFeed, totalOfPrices, totalShares}
 }
 
+//totalShares
 
-export function wssTick (changes, state, lastSplit, lastMid) {
-
+export function wssTick (changes, state, lastSplit, lastMid, sorted, totalOfPrices, totalShares) {
+  let length = sorted.length
   let newState = {...state}
   let splitIndex;
   let midPrice;
-  let totalShares;
+  let totalNumShares;
+  let newSortedFeed = [...sorted];
   let price = (+changes[1]*100).toFixed()
   let size = (+changes[2]*100).toFixed()
-  console.log('hello', size)
-
   if(state[price]){
       if(+size === 0){
-        delete newState[price]
+        let { newTotalPrice, newTotalShares, newMid } = findMedian(totalOfPrices, totalShares, [price, state[price]], [price, size], state.length, lastMid,  'delete')
+        let old = {...state}
+        let {price, ...updated} = old;
+        newSortedFeed = sorted.filter(bid => +bid[0] != price)
+        midPrice = newMid
+        totalNumShares = newTotalShares
+        totalOfPrices = newTotalPrice
+        length--
+        newState = updated
       }else{
           newState[price] = size
+          let { newTotalPrice, newTotalShares, newMid } = findMedian(totalOfPrices, totalShares, [price, state[price]], [price, size], state.length, lastMid,  'update')
+          midPrice = newMid
+          totalNumShares = newTotalShares
+          totalOfPrices = newTotalPrice
       }
-  }else if(+size !== 0){
+  }else{
       newState[price] = size
+      let newSortedFeed =  insertBid(sorted, price)
+      let { newTotalPrice, newTotalShares, newMid } = findMedian(totalOfPrices, totalShares, [price, state[price]], [price, size], state.length, lastMid,  'add')
+      length++
+      midPrice = newMid
+      totalNumShares = newTotalShares
+      totalOfPrices = newTotalPrice
   }
-  let prices = Object.keys(newState)
-  midPrice = prices.reduce((a, b) => {return +a + (+b)})/prices.length
-  splitIndex = Object.keys(newState).length/2
-  totalShares = Object.values(newState).reduce((a, b) => {return +a + (+b)})/prices.length
-
-  return {splitIndex, newState, totalShares, newMidPrice: midPrice}
+  return {splitIndex: length/2, newState, newMidPrice: midPrice, sortedFeed: newSortedFeed, totalOfPrices}
 }
+
+
+
+
 
 
 
